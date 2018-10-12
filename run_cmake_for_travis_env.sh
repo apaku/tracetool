c1eda6e69def6b2ce4e8b8fd3a206394dab04f65
Andreas Pakulat <andreas@froglogic.com>
2018-10-12 18:50:27 +0200

    fixup! run_cmake

--- run_cmake_for_travis_env.sh
+++ run_cmake_for_travis_env.sh
@@ -6,12 +6,11 @@ if [[ "x$TRAVIS_TAG" == "x" ]] ; then
 else
     RPATH_ARG=""
     DEPLOYMENT_TARGET_ARG=""
-    if [[ $TRAVIS_OS_NAME == "linux" ]] ; then
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]] ; then
         RPATH_ARG="-DCMAKE_INSTALL_RPATH=\\\$ORIGIN/../lib"
     else
-        RPATH_ARG="-DENABLE_INSTALL_RPATH=OFF"
         DEPLOYMENT_TARGET_ARG="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.10"
     fi
-    cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLE_QT=ON "$RPATH_ARG" "$DEPLOYMENT_TARGET_ARG" $@
+    cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLE_QT=ON -DENABLE_INSTALL_RPATH=OFF "$RPATH_ARG" "$DEPLOYMENT_TARGET_ARG" $@
 fi
 
