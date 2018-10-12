18874f60bd576fb1595058045079e94150ab816b
Andreas Pakulat <andreas@froglogic.com>
2018-10-12 17:29:06 +0200

    Build against an older SDK than is available on the mac
    This ensures compatibility with older macOS versions, in particular on
    travis the default minimum sdk version is 10.13, leaving anybody with older
    macos to be unable to use the tools.

--- run_cmake_for_travis_env.sh
+++ run_cmake_for_travis_env.sh
@@ -5,11 +5,13 @@ if [[ "x$TRAVIS_TAG" == "x" ]] ; then
     cmake -DCMAKE_BUILD_TYPE=Debug $@
 else
     RPATH_ARG=""
+    DEPLOYMENT_TARGET_ARG=""
     if [[ $TRAVIS_OS_NAME == "linux" ]] ; then
         RPATH_ARG="-DCMAKE_INSTALL_RPATH=\\\$ORIGIN/../lib"
     else
         RPATH_ARG="-DENABLE_INSTALL_RPATH=OFF"
+        DEPLOYMENT_TARGET_ARG="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
     fi
-    cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLE_QT=ON "$RPATH_ARG" $@
+    cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLE_QT=ON "$RPATH_ARG" "$DEPLOYMENT_TARGET_ARG" $@
 fi
 
