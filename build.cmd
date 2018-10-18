e0d8816dd19af150c42ec32f80e52ba34a532c91
Andreas Pakulat <andreas@froglogic.com>
2018-10-18 23:06:59 +0200

    use arch lib suffix for windows

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
+@echo on
 if "%~1" == "" goto usage
 if "%~2" == "" goto usage
 
@@ -16,18 +17,20 @@ if "%~1" == "x86" goto x86 else (
 :x86
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 set prefixPath=C:\Qt\5.9.5\msvc2015
+set archSuffix="x86"
 goto build
 
 :x64
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 set prefixPath=C:\Qt\5.9.5\msvc2015_64
+set archSuffix="x64"
 goto build
 
 :build
 mkdir build
 pushd build
-cmake -DCMAKE_PREFIX_PATH=%prefixPath% -DCMAKE_BUILD_TYPE=%buildType% -G "Ninja" ..
+cmake -DARCH_LIB_SUFFIX=%archSuffix% -DCMAKE_PREFIX_PATH=%prefixPath% -DCMAKE_BUILD_TYPE=%buildType% -G "Ninja" ..
 ninja
 goto exit
 
